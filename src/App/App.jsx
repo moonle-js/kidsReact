6cc7131bd589b1521fd7736dfd926ee774d3815d
Mahammad <mehemmedabbasov123@gmail.com>
2024-05-03 19:19:13 +0400

    Adding axios library

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,21 @@
 import { Outlet } from "react-router";
 import Header from "../Header/Header";
+import axios from "axios";
+
 
 export default function App(){
+
+    // async function goster(){
+    //     try{
+    //         var response = await axios.get('/data.json')
+    //         console.log(response.data)
+    //     }catch(error){
+    //       console.log(error)
+    //     }
+    // }
+
+    // goster()
+
     return(
         <>
             <Header></Header>
